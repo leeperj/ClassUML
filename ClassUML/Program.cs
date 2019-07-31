354ec153d9660af888f4af47400ab4a5485413f1
unknown <josephleeper@gmail.com>
2019-07-31 09:46:06 -0400

    updated with add

--- ClassUML/Program.cs
+++ ClassUML/Program.cs
@@ -19,6 +19,9 @@ namespace ClassUML
                 new Staff("Donald Simmons","\t664 Elm St.","Harvard",75000)
 
             };
+            bool goAgain = true;
+            while (goAgain)
+            { 
 
             foreach (var item in person)
             {
@@ -27,7 +30,68 @@ namespace ClassUML
                 
             }
 
-            
+            Console.WriteLine("Would you like to add someone to the list?");
+            string userInput = Console.ReadLine().ToLower().Trim();
+
+                if (userInput == "yes" || userInput == "y")
+                {
+                    
+                    Console.WriteLine("Is this a persone going to be a student or staff member?");
+                    string staffStudent = Console.ReadLine().ToLower().Trim();
+                    if (staffStudent == "staff")
+                    {
+                        Console.WriteLine("Please enter staff member Name.");
+                        string staffName = Console.ReadLine();
+
+                        Console.WriteLine("Please enter staff member Address.");
+                        string staffAddress = Console.ReadLine();
+
+                        Console.WriteLine("Please enter School where this staff member works.");
+                        string staffSchool = Console.ReadLine();
+
+                        Console.WriteLine("How much does this staff member make?");
+                        double staffPay = double.Parse(Console.ReadLine());
+
+                        person.Add(new Staff(staffName, staffAddress, staffSchool, staffPay));
+                    }
+                    else if (staffStudent == "student")
+                    {
+                        Console.WriteLine("Please enter subject this student is studying.");
+                        string studentSubject = Console.ReadLine();
+
+                        Console.WriteLine("Please enter year this student is graduating.");
+                        int studentGrad = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Please enter how much this student is paying.");
+                        double studentPay = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Please enter student name.");
+                        string studentName = Console.ReadLine();
+
+                        Console.WriteLine("Please enter student address.");
+                        string studentAddress = Console.ReadLine();
+
+                        person.Add(new Student(studentSubject, studentGrad, studentPay, studentName, studentAddress));
+                    }
+                    else
+                    {
+                        Console.WriteLine("That was not a proper input, please type student or staff.");
+                    }
+
+                }
+
+                string quitChoice;
+                Console.WriteLine("Enter (Q)uit to exit, or any other key to run again");
+
+                quitChoice = Console.ReadLine().Trim();
+
+                if (quitChoice.ToLower() == "q" || quitChoice.ToLower() == "quit")
+                {
+                    goAgain = false;
+                    Console.WriteLine("Have a nice day!");
+                }
+
+            }
 
         }
     }
